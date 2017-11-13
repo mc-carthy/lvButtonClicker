1cb6cffa551d8215f9488dd69f92e775f775b486
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-11-13 22:09:06 +0000

    Adding colour

--- main.lua
+++ main.lua
@@ -8,6 +8,13 @@ end
 
 -- This draws graphics to the screen, runs at 60fps default
 function love.draw()
+  -- x, y, width, height are all in pixels
+  -- left and down are positive
+  -- pivot is upper right corner
+  love.graphics.setColor(191, 0, 0)
   love.graphics.rectangle("fill", 0, 0, 200, 100)
+  love.graphics.setColor(191, 0, 191)
   love.graphics.rectangle("line", 0, 100, 200, 100)
+  love.graphics.setColor(0, 191, 191)
+  love.graphics.circle("fill", 400, 400, 200)
 end
