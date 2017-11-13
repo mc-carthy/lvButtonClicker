45789011d20f947b9de1c7601cc3262def9b9f24
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-11-13 22:20:05 +0000

    Clicking button increases score

--- main.lua
+++ main.lua
@@ -1,5 +1,14 @@
 -- This runs as soon as the game loads
 function love.load()
+  button = {}
+  button.x = 200
+  button.y = 200
+  button.radius = 50
+
+  score = 0
+  timer = 0
+
+  myFont = love.graphics.newFont(40)
 end
 
 -- This is the game loop, default is 60fps
@@ -8,13 +17,24 @@ end
 
 -- This draws graphics to the screen, runs at 60fps default
 function love.draw()
-  -- x, y, width, height are all in pixels
-  -- left and down are positive
-  -- pivot is upper right corner
-  love.graphics.setColor(191, 0, 0)
-  love.graphics.rectangle("fill", 0, 0, 200, 100)
-  love.graphics.setColor(191, 0, 191)
-  love.graphics.rectangle("line", 0, 100, 200, 100)
   love.graphics.setColor(0, 191, 191)
-  love.graphics.circle("fill", 400, 400, 200)
+  love.graphics.circle("fill", button.x, button.y, button.radius)
+
+  love.graphics.setFont(myFont)
+  love.graphics.setColor(255, 255, 255)
+  love.graphics.print(score)
+end
+
+function love.mousepressed(x, y, btn, isTouch)
+  -- 1 = left button, 2 = right button, 3 = middle button
+  if (btn == 1) then
+    -- if (distanceBetweenPoints(x, y, button.x, button.y) < button.radius) then
+    if (distanceBetweenPoints(love.mouse.getX(), love.mouse.getY(), button.x, button.y) < button.radius) then
+      score = score + 1
+    end
+  end
+end
+
+function distanceBetweenPoints(x1, y1, x2, y2)
+  return math.sqrt((y2 - y1)^2 + (x2 - x1)^2)
 end
