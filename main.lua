2271af086580302c353de73125d930764b45d187
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-11-13 22:05:10 +0000

    Drawing test

--- main.lua
+++ main.lua
@@ -1,14 +1,13 @@
 -- This runs as soon as the game loads
 function love.load()
-  number = 0
 end
 
 -- This is the game loop, default is 60fps
 function love.update(dt)
-  number = number + 1
 end
 
 -- This draws graphics to the screen, runs at 60fps default
 function love.draw()
-  love.graphics.print(number)
+  love.graphics.rectangle("fill", 0, 0, 200, 100)
+  love.graphics.rectangle("line", 0, 100, 200, 100)
 end
