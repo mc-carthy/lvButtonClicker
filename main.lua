d8eee3fbc0cdd68ac07cfbd684b191aac4a1a571
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-11-13 22:00:30 +0000

    Main functions test

--- main.lua
+++ main.lua
@@ -1,12 +1,14 @@
 -- This runs as soon as the game loads
 function love.load()
+  number = 0
 end
 
 -- This is the game loop, default is 60fps
 function love.update(dt)
+  number = number + 1
 end
 
 -- This draws graphics to the screen, runs at 60fps default
 function love.draw()
-
+  love.graphics.print(number)
 end
