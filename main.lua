7f93be6536835e3c6daf8aae85e669ba08ae4dbe
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-11-13 21:59:31 +0000

    Init repo

---
+++ main.lua
@@ -0,0 +1,12 @@
+-- This runs as soon as the game loads
+function love.load()
+end
+
+-- This is the game loop, default is 60fps
+function love.update(dt)
+end
+
+-- This draws graphics to the screen, runs at 60fps default
+function love.draw()
+
+end
